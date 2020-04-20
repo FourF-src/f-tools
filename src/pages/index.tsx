dde042fae32344e647a36dcad6b767e94b11cadd
author <author@fourf>
2020-04-20 23:52:19 +0800

    feat: save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Card } from 'antd-mobile';
 import Link from 'umi/link';
+import * as style from './index.less';
 export default function () {
   return <>
-    <Card >
+    <Card className={style.card}>
       <Card.Header
         title="ETFs"
         thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
@@ -13,6 +14,17 @@ export default function () {
         <Link to="/etf">ETF's chart and info </Link>
       </Card.Body>
       <Card.Footer extra={<Link to="/etf">view detail</Link>} />
+    </Card>
+    <Card className={style.card}>
+      <Card.Header
+        title="Stock"
+        thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
+        extra={<span>stock research</span>}
+      />
+      <Card.Body>
+        <Link to="/stock">stock research </Link>
+      </Card.Body>
+      <Card.Footer extra={<Link to="/stock">view detail</Link>} />
     </Card>
 
   </>
---
+++ src/pages/stock/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index:React.FC=p=>{
+    return <div>stock</div>
+};
+
+export default Index;
