4baf3fb58cc70de55a45d0f93a2d97feabb55077
wanghsinche.wxz <wanghsinche.wxz@alibaba-inc.com>
2020-05-12 15:29:56 +0800

    fix: 添加fallback

--- api/main.ts
+++ api/main.ts
@@ -10,7 +10,21 @@ app.use(bodyParser.json())
 app.use('/api/etf', fund);
 app.use('/api/stock', stock);
 
-app.use(express.static(path.resolve(__dirname, '../dist')));
+const serveStatic = express.static(path.resolve(__dirname, '../dist'));
+app.use(serveStatic);
+
+// fallback
+// All GET request handled by INDEX file
+app.get('*', function (req, res, next) {
+    const host = 'http://temp';
+    const url = new URL(req.url, host);
+    url.pathname = '/';
+    req.url = url.href.replace(host, '');
+    serveStatic(req, res, next);
+});
+  
+
+
 app.listen(9000)
 
 console.log('api run at http://localhost:9000')
